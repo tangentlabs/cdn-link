be7495d5c4a0a00c582493ebba1d1abc0446d0dc
Richard Noble <ranoble@gmail.com>
2016-08-23 15:06:31 +0100

    all things to cdnlink

--- module/actions/cdnlink/cdnlink.js
+++ module/actions/cdnlink/cdnlink.js
@@ -27,11 +27,14 @@ define(function(require, exports, module) {
 
             var self = this;
             console.log('execute');
-            alert('asdasdas');
+            var project = actionContext.observable("project").get();
+            var branch = actionContext.observable("branch").get();
+            var document = actionContext.observable("document").get();
+            console.log(document);
 
             UI.showPopupModal({
                 "title": config.title,
-                "body": "This is the content for the model"
+                "body": "<p>Your CDN link for production is: <a href=\"https://d1iyvc6w63ago2.cloudfront.net/crop/force/644/430/935cd0f14e6423d06b86/SB_322x215px_LOB_PUBS1.jpg\">https://d1iyvc6w63ago2.cloudfront.net/crop/force/644/430/935cd0f14e6423d06b86/SB_322x215px_LOB_PUBS1.jpg</a></p><p>Your link for stage is <a href=\"https://d1iyvc6w63ago2.cloudfront.net/crop/force/644/430/935cd0f14e6423d06b86/SB_322x215px_LOB_PUBS1.jpg\">https://d1iyvc6w63ago2.cloudfront.net/crop/force/644/430/935cd0f14e6423d06b86/SB_322x215px_LOB_PUBS1.jpg</a>(this will change with branches...)"
             }, function(modalDiv) {
                 // TODO: add any post-render logic here to manipulate the modal div
             });
